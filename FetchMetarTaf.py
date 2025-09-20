00504d52c6a3fdcdedf4717259b85cf3c9bf72c0
Jordi Warmenhoven <jordi.warmenhoven@gmail.com>
2025-09-20 09:52:16 +0200

    Change to XML download
    TAF cache file in CSV format does not seem to be available for download anymore. Switching to reading XML files instead. Also implemented more robust exception handling.

--- FetchMetarTaf.py
+++ FetchMetarTaf.py
@@ -6,8 +6,10 @@ pd.options.display.max_colwidth = 250
 
 
 import syslog
+import traceback
 import locale
 import weewx.reportengine
+from urllib.error import URLError
 
 def logmsg(lvl, msg):
     syslog.syslog(lvl, 'FetchMetarTaf: {}'.format(msg))
@@ -18,6 +20,11 @@ def loginf(msg):
 def logerr(msg):
     logmsg(syslog.LOG_ERR, msg)
 
+def log_exception(context, exception):
+    """Helper function to log detailed exception info."""
+    logerr(f"{context}: {exception}")
+    logerr(traceback.format_exc())
+
 
 class MetarTafLoader(weewx.reportengine.ReportGenerator):
     """Class for downloading metar & taf records"""
@@ -25,7 +32,6 @@ class MetarTafLoader(weewx.reportengine.ReportGenerator):
 
     def run(self):
         """Main entry point for downloading records"""
-
         self.metar_url = self.skin_dict['FetchMetarTaf']['metar_url']
         self.taf_url = self.skin_dict['FetchMetarTaf']['taf_url']
         self.stations = self.skin_dict['FetchMetarTaf']['stations']
@@ -37,58 +43,50 @@ class MetarTafLoader(weewx.reportengine.ReportGenerator):
         self._create_dataframe()
 
     def _create_dataframe(self):
-        """Create pandas DataFrame"""
+        """Create pandas DataFrame and write to HTML"""
 
-        # Write DataFrame
         stations_filter = [s.strip() for s in self.stations]
-#        metar_subset = self.metar.loc[self.metar.index.intersection(stations_filter)]
-        metar_subset = self.metar.drop_duplicates().reindex(stations_filter).dropna()
-#        taf_subset = self.taf.loc[self.taf.index.intersection(stations_filter)]
-        taf_subset = self.taf.drop_duplicates().reindex(stations_filter).dropna()
 
         try:
+            metar_subset = self.metar.drop_duplicates().reindex(stations_filter).dropna()
             metar_subset.to_html(self.metar_dataframe_path,
                     header=False,
                     index=False,
                     border=0,
                     table_id='metar'
                     )
-        except:
-            logerr('FetchMetarTaf: Could not write dataframe: {}'.format(self.metar_dataframe_path))
+        except (AttributeError, OSError, ValueError) as e:
+            log_exception(f"Could not write METAR dataframe to {self.metar_dataframe_path}", e)
  
         try:
+            taf_subset = self.taf.drop_duplicates().reindex(stations_filter).dropna()
             taf_subset.to_html(self.taf_dataframe_path,
                     header=False,
                     index=False,
                     border=0,
                     table_id='taf'
                     )
-        except:
-            logerr('FetchMetarTaf: Could not write dataframe: {}'.format(self.taf_dataframe_path))
-
-
+        except (AttributeError, OSError, ValueError) as e:
+            log_exception(f"Could not write TAF dataframe to {self.taf_dataframe_path}", e)
 
 
     def _download_records(self):
         """Download records from textserver"""
 
         try:
-            self.metar = pd.read_csv(self.metar_url,
-                    skiprows=5,
-                    index_col=1,
-                    usecols=[0,1]
-                    ).sort_index()
-        except:
-            logerr('FetchMetarTaf: Unable to download METAR records from {}.'.format(self.metar_url))
+            self.metar = pd.read_xml(self.metar_url, xpath=".//METAR"
+                                     ).set_index('station_id').sort_index()['raw_text'].to_frame()
+        except (pd.errors.EmptyDataError, pd.errors.ParserError, URLError, FileNotFoundError) as e:
+            log_exception(f"Unable to download METAR records from {self.metar_url}", e)
+            self.metar = pd.DataFrame(columns=['raw_text']) # fallback to empty DF
 
         try:
-            self.taf = pd.read_csv(self.taf_url,
-                    skiprows=6,
-                    header=None,
-                    index_col=1,
-                    usecols=[0,1],
-                    names=['raw_text', 'station_id']
-                    ).sort_index()
-        except:
-            logerr('FetchMetarTaf: Unable to download TAF records from {}.'.format(self.taf_url))
+            self.taf = pd.read_xml(self.taf_url, xpath=".//TAF"
+                                   ).set_index('station_id').sort_index()['raw_text'].to_frame()
+        except (pd.errors.EmptyDataError, pd.errors.ParserError, URLError, FileNotFoundError) as e:
+            log_exception(f"Unable to download TAF records from {self.taf_url}", e)
+            self.taf = pd.DataFrame(columns=['raw_text']) # fallback to empty DF
+
+
+
 
